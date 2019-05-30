95fed7fcb1a9398d4464cc84c0ffef1af202b50d
piladzisi <piladzisi@gmail.com>
2019-05-30 17:15:27 +0300

    splashColor: Colors.transparent,   highlightColor: Colors.transparent, // makes highlight invisible too

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MyApp());
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlatButton(
+            //splashColor: Colors.transparent,
+            highlightColor: Colors.transparent, // makes highlight invisible too
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[800],
+          title: Center(child: Text('Ask Me Anything')),
+        ),
+        backgroundColor: Colors.blue,
+        body: Ball(),
+      ),
+    );
+  }
+}
